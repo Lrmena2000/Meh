62ff849afe6ffb17bd51bec46177c5570a876140
AlsoloProg <alexandersolislopez@gmail.com>
2021-02-27 22:00:07 -0600

    Union de ambas pares

---
+++ funcion.py
@@ -0,0 +1,52 @@
+def check_char (char):
+    tipo = type(char)
+    codigo = 0
+    rango = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    if tipo == str:
+        tam = len (char)
+        char = char.upper()
+        if tam >1:
+            cont1 = 0 
+            ctrl = 0 
+
+            while ctrl == 0:
+                for lett in char:
+                    cont1 = cont1 +1
+                    if codigo == 2 and cont1 == tam +1:
+                        ctrl = 1 
+                        break
+                    if cont1 == tam +1:
+                        codigo = 1
+                        ctrl = 1
+                        break
+                    for alph in rango:
+                        if lett == alph:
+                            codigo = 1
+                            break
+                        if alph == "Z" and lett != alph:
+                            ctrl = 1
+                            codigo = 2
+                            cont1 = tam
+        else:
+            for alph in rango:
+                        if char == alph:
+                            codigo = 0
+                            break
+                        if alph == "Z" and char != alph:
+                            codigo = 2
+
+    else:
+        codigo = 3
+    return codigo
+prueba = check_char()
+print (prueba)
+
+def cap_switch(char_in):
+    if char_in.isupper():
+        char_in = char_in.lower()
+    elif char_in.islower():
+        char_in = char_in.upper()
+    return char_in
+
+
+print(cap_switch("a"))
